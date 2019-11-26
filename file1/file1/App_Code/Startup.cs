1d57c226f8661ce6302962e487cad3e20e523db8
minakh829 <57958659+minakh829@users.noreply.github.com>
2019-11-26 14:40:36 +0100

    file

---
+++ file1/file1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(file1.Startup))]
+namespace file1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
